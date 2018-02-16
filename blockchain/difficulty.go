66ec8d3909341faffd44ef74e7dd76d8bac4c163
bogrod <john@secureware.io>
2018-02-15 20:07:12 -0800

    blockchain: Revert Consensus Deployments

--- blockchain/difficulty.go
+++ blockchain/difficulty.go
@@ -982,7 +982,18 @@ func (b *BlockChain) calcNextRequiredStakeDifficulty(curNode *blockNode) (int64,
 	// NOTE: The choice field of the return threshold state is not examined
 	// here because there is only one possible choice that can be active
 	// for the agenda, which is yes, so there is no need to check it.
-	return b.calcNextRequiredStakeDifficultyV2(curNode)
+	deploymentVersion := sdiffAlgoDeploymentVersion(b.chainParams.Net)
+	state, err := b.deploymentState(curNode, deploymentVersion,
+		chaincfg.VoteIDSDiffAlgorithm)
+	if err != nil {
+		return 0, err
+	}
+	if state.State == ThresholdActive {
+		return b.calcNextRequiredStakeDifficultyV2(curNode)
+	}
+
+	// Use the old stake difficulty algorithm in any other case.
+	return b.calcNextRequiredStakeDifficultyV1(curNode)
 }
 
 // CalcNextRequiredStakeDifficulty calculates the required stake difficulty for
@@ -1471,7 +1482,19 @@ func (b *BlockChain) estimateNextStakeDifficulty(curNode *blockNode, newTickets 
 	// NOTE: The choice field of the return threshold state is not examined
 	// here because there is only one possible choice that can be active
 	// for the agenda, which is yes, so there is no need to check it.
-	return b.estimateNextStakeDifficultyV2(curNode, newTickets,
+	deploymentVersion := sdiffAlgoDeploymentVersion(b.chainParams.Net)
+	state, err := b.deploymentState(curNode, deploymentVersion,
+		chaincfg.VoteIDSDiffAlgorithm)
+	if err != nil {
+		return 0, err
+	}
+	if state.State == ThresholdActive {
+		return b.estimateNextStakeDifficultyV2(curNode, newTickets,
+			useMaxTickets)
+	}
+
+	// Use the old stake difficulty algorithm in any other case.
+	return b.estimateNextStakeDifficultyV1(curNode, newTickets,
 		useMaxTickets)
 }
 
