f382a32d13a15238004f199cc684ffa444f82860
bogrod <john@secureware.io>
2018-02-24 23:16:28 -0800

    chaincfg: Update BlockOneLedgerTestNet2

--- chaincfg/premine.go
+++ chaincfg/premine.go
@@ -18,7 +18,7 @@ var BlockOneLedgerTestNet = []*TokenPayout{
 // BlockOneLedgerTestNet2 is the block one output ledger for the 2nd test
 // network.
 var BlockOneLedgerTestNet2 = []*TokenPayout{
-	{"Tsf2TGFxc83pWbTE1JhNiwiX4uNHkSp6a5G", 100000 * 1e8},
+	{"Tsf2TGFxc83pWbTE1JhNiwiX4uNHkSp6a5G", 200000000 * 1e8},
 }
 
 // BlockOneLedgerSimNet is the block one output ledger for the simulation
